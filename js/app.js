72475adfa9d302800dc57bbc3c13e7271a956c8a
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-10-11 19:45:44 +0600

    delete single data in tbody

--- js/app.js
+++ js/app.js
@@ -7,26 +7,41 @@ const inputFunBtn = () => {
   const inputValue = inputField.value;
 
    // index number
-   let num = 0;
+   
 
   //check input value empty
   if(inputValue !== ""){
       // set input value in table
-  let tableData = document.getElementById('tableData');
-  let tr = document.createElement('tr');
-  tr.innerHTML = `
-  <th>${++num}</th>
-  <td>
-    <input type="checkbox" class="checkbox" />
-  </td>
-  <td>${inputValue}</td>
-  <td><button class="btn btn-sm bg-red-500 text-white hover:bg-red-500">Small</button></td>
-  `
-  tableData.appendChild(tr);
-
-  inputField.value = "";
+      let tableData = document.getElementById('tableData');
+      let tr = document.createElement('tr');
+      tr.innerHTML = `
+      <th>#</th>
+      <td>
+        <input type="checkbox" class="checkbox" />
+      </td>
+      <td>${inputValue}</td>
+      <td><button onclick="deleteFunBtn(this)" class="btn btn-sm bg-red-500 text-white hover:bg-red-500">Small</button></td>
+      `
+      // append the tr
+      tableData.appendChild(tr);
+      // clear input value
+      inputField.value = "";
   }else{
     alert("Please enter some data")
   }
 }
 
+// delete single data
+const deleteFunBtn = (btnData) => {
+
+  // select button parentNode data
+  let td = btnData.parentNode;
+  let tr = td.parentNode;
+  let tbody = tr.parentNode;
+  tbody.removeChild(tr);
+
+  // success message
+  alert('successfully deleted data')
+
+
+}
